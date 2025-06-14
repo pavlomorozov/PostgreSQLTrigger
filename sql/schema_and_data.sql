1cded9fdb6c0f3af3655b0edf31e23062383e88e
Pavlo Morozov <6754248+pavlomorozov@users.noreply.github.com>
2025-06-14 16:19:50 +0200

    Sql to create new table and fill it with data.

---
+++ sql/schema_and_data.sql
@@ -0,0 +1,17 @@
+-- Create new table with initial data. If table already exists - drop it first.
+
+DROP TABLE IF EXISTS public.hero;
+
+CREATE TABLE public.hero (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL
+);
+
+INSERT INTO public.hero (first_name, last_name) VALUES
+('Jim', 'Hawkins'),
+('Captain', 'Nemo'),
+('Phileas', 'Fogg'),
+('Huckleberry', 'Finn'),
+('Edmond', 'Dantès'),
+('Robinson', 'Crusoe');
