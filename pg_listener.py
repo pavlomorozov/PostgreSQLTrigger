f7933a905facb8379cd941439c56534dff90d135
Pavlo Morozov <6754248+pavlomorozov@users.noreply.github.com>
2025-06-15 15:39:32 +0200

    comment on pg_listener

--- pg_listener.py
+++ pg_listener.py
@@ -4,14 +4,11 @@ import asyncpg
 async def listen():
     conn = await asyncpg.connect("postgresql://postgres:postgres@localhost/callback")
     await conn.add_listener('hero_update', handler)
+
+    # wait for user input to quit running script
     loop = asyncio.get_running_loop()
     stop_event = asyncio.Event()
-
-    def stop():
-        stop_event.set()
-
-    loop.add_reader(0, lambda: stop() if input().strip() == 'exit' else None)
-
+    loop.add_reader(0, lambda: stop_event.set() if input().strip() == 'exit' else None)
     await stop_event.wait()
     await conn.close()
 
